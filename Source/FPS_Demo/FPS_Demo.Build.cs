67a526c440131a17f2e3f2eae353afcd75c0b664
jinannan <372422820@qq.com>
2021-02-26 17:39:47 +0800

    UMG测试

--- Source/FPS_Demo/FPS_Demo.Build.cs
+++ Source/FPS_Demo/FPS_Demo.Build.cs
@@ -8,7 +8,7 @@ public class FPS_Demo : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
